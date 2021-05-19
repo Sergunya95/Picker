5577cb813e16eb20852467df7ba0842279a2ca72
sergunya95 <sergunya95@gmail.com>
2021-05-19 17:16:50 +0700

    PickerView on SwiftUI

--- Picker/ContentView.swift
+++ Picker/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selection = 0
+    @State var isOn = false
+    
+    var settingsTime = [ "1 min",
+                         "2 min",
+                         "3 min",
+                         "5 min"]
+    
+    var colors = [ "Red",
+                   "Green",
+                   "Blue",
+                   "Pink"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            Form {
+                
+                Picker(selection: $selection,
+                       label: Text("Time"),
+                       content: {
+                        ForEach(0..<settingsTime.count) {
+                            Text(self.settingsTime[$0])
+                        }
+                       })
+                
+                Toggle(isOn: $isOn, label: {
+                    Text("Airplane mode").foregroundColor(isOn ? .black : .orange)
+                })
+                
+                Picker(selection: $selection,
+                       label: Text("Color").foregroundColor(.red),
+                       content: {
+                        ForEach(0..<colors.count) {
+                            Text(self.colors[$0])
+                        }
+                       })
+                
+            }.navigationBarTitle("Settings")
+        }
     }
 }
 
